348d911c5cc0250df1e55a576d57447ae58938c0
AriiiiDkCode <m.harith.hadi@gmail.com>
2025-06-14 14:46:59 +0800

    "react/react-in-jsx-scope": "off",

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig([
     rules: {
       // Turn off the newline-before-return rule
       "style/newline-before-return": "off",
+      "react/react-in-jsx-scope": "off",
     },
   },
   tseslint.configs.recommended,
