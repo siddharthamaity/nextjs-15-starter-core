ddd4d920fad44a9dfd7fa7b10bb255046d88bde0
Siddhartha Maity <siddharthamaity1993@gmail.com>
2025-10-10 14:25:02 +0530

    chore: update recommended extensions in VSCode configuration

--- src/app/(delete-this-and-modify-page.tsx)/ExtensionDetails.tsx
+++ src/app/(delete-this-and-modify-page.tsx)/ExtensionDetails.tsx
@@ -3,22 +3,22 @@ import Link from 'next/link';
 import './ExtensionDetails.css';
 
 const RECOMMENDED_EXTENSIONS: string[] = [
-    'formulahendry.auto-close-tag',
-    'aaron-bond.better-comments',
-    'mikestead.dotenv',
-    'EditorConfig.EditorConfig',
-    'dbaeumer.vscode-eslint',
-    'MikeBovenlander.formate',
-    'donjayamanne.githistory',
-    'wix.vscode-import-cost',
-    'sburg.vscode-javascript-booster',
-    'christian-kohler.npm-intellisense',
-    'esbenp.prettier-vscode',
-    'Gruntfuggly.todo-tree',
-    'ChakrounAnas.turbo-console-log',
-    'codeandstuff.package-json-upgrade',
-    'KnisterPeter.vscode-commitizen',
-    'yzhang.markdown-all-in-one'
+    'formulahendry.auto-close-tag', // ? https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-close-tag
+    'aaron-bond.better-comments', // ? https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments
+    'mikestead.dotenv', // ? https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig
+    'EditorConfig.EditorConfig', // ? https://marketplace.visualstudio.com/items?itemName=EditorConfig.EditorConfig
+    'dbaeumer.vscode-eslint', // ? https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
+    'MikeBovenlander.formate', // ? https://marketplace.visualstudio.com/items?itemName=MikeBovenlander.formate
+    'donjayamanne.githistory', // ? https://marketplace.visualstudio.com/items?itemName=donjayamanne.githistory
+    'wix.vscode-import-cost', // ? https://marketplace.visualstudio.com/items?itemName=wix.vscode-import-cost
+    'sburg.vscode-javascript-booster', // ? https://marketplace.visualstudio.com/items?itemName=sburg.vscode-javascript-booster
+    'christian-kohler.npm-intellisense', // ? https://marketplace.visualstudio.com/items?itemName=christian-kohler.npm-intellisense
+    'esbenp.prettier-vscode', // ? https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+    'ChakrounAnas.turbo-console-log', // ? https://marketplace.visualstudio.com/items?itemName=ChakrounAnas.turbo-console-log
+    'codeandstuff.package-json-upgrade', // ? https://marketplace.visualstudio.com/items?itemName=codeandstuff.package-json-upgrade
+    'yzhang.markdown-all-in-one', // ? https://marketplace.visualstudio.com/items?itemName=yzhang.markdown-all-in-one,
+    'bradlc.vscode-tailwindcss', // ? https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
+    'stivo.tailwind-fold' // ? https://marketplace.visualstudio.com/items?itemName=stivo.tailwind-fold
 ];
 
 interface ExtensionStatistics {
